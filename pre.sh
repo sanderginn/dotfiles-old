bf5a7e00aa349ef005ede31f2a928921285ef449
Sander Ginn <sanderginn@users.noreply.github.com>
2020-02-11 10:05:19 +0100

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -18,3 +18,5 @@ git submodule add https://github.com/anishathalye/dotbot
 git config -f .gitmodules submodule.dotbot.ignore dirty # ignore dirty commits in the submodule
 cp dotbot/tools/git-submodule/install .
 touch install.conf.yaml
+
+rm ~/pre.sh
