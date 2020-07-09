d706a864fdb2a27851d60db871c534e87aaf944d
Sander Ginn <sander@ginn.it>
2020-07-09 11:04:33 +0200

    More changes

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,3 +3,4 @@ alias terraformk8s='docker run -v ${HOME}/.kube/config:/kubeconfig -e KUBECONFIG
 alias terraformk8strace='docker run -v ${HOME}/.kube/config:/kubeconfig -e KUBECONFIG=/kubeconfig -e TF_LOG=TRACE -v `pwd`:/docker -w /docker --rm -it registry.rebelsoft.com/terraform-provider-k8s terraform'
 alias tfgenerate='docker run -v ${HOME}/.kube/config:/kubeconfig -e KUBECONFIG=/kubeconfig -v `pwd`:/docker -w /docker --rm -it registry.rebelsoft.com/terraform-provider-k8s generator'
 alias tfextract='docker run -v ${HOME}/.kube/config:/kubeconfig -e KUBECONFIG=/kubeconfig -v `pwd`:/docker -w /docker --rm -it registry.rebelsoft.com/terraform-provider-k8s extractor'
+alias clearremotestate="terraform state list | cut -f 1 -d '[' | xargs -L 1 terraform state rm"
